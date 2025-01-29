fdcc351df9b89eed36d847edceddc4d9648cbba6
Abhay <abhay69123@gmail.com>
2025-01-29 17:51:46 +0530

    First Commit

---
+++ src/main/java/com/PractiseApi/PractiseApi1Application.java
@@ -0,0 +1,13 @@
+package com.PractiseApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PractiseApi1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PractiseApi1Application.class, args);
+	}
+
+}
